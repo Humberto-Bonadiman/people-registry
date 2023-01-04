fd6fed08d9f3b2c0a8bede39f4435cd9186e0466
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2023-01-04 00:32:16 -0400

    feat: create repositories

---
+++ src/main/java/com/spring/register/people/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.spring.register.people.repository;
+
+import com.spring.register.people.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findByMainAddress(boolean mainAddress);
+}
---
+++ src/main/java/com/spring/register/people/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.spring.register.people.repository;
+
+import com.spring.register.people.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
