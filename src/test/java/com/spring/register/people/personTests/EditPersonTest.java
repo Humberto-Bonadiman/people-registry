425b0f39d142d3cf6e47b6443981e09b42fd8e1e
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2023-01-04 20:35:49 -0400

    test: create tests to edit a person

--- src/test/java/com/spring/register/people/personTests/EditPersonTest.java
+++ src/test/java/com/spring/register/people/personTests/EditPersonTest.java
@@ -11,13 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CreatePersonTest {
+public class EditPersonTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -36,36 +38,54 @@ public class CreatePersonTest {
 
     @Test
     @Order(1)
-    @DisplayName("1 - Must register a person successfully")
-    void createPersonSuccessfully() throws Exception {
-        final PersonTestDto person = CreatePerson.createPersonTestDto();
-        mockMvc.perform(post("/person")
+    @DisplayName("1 - Must edit a person successfully")
+    void editPersonSuccessfully() throws Exception {
+        Person person = CreatePerson.createTestPerson();
+        personRepository.save(person);
+        PersonTestDto personTestDto = CreatePerson.createPersonTestDto();
+        mockMvc.perform(put("/person/" + person.getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(person)))
+                        .content(new ObjectMapper().writeValueAsString(personTestDto)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isOk());
     }
 
     @Test
     @Order(2)
-    @DisplayName("2 - Must throw a error without the birth date")
-    void badRequestWithoutBirthDate() throws Exception {
-        final Person person = new Person("Bad Request");
-        mockMvc.perform(post("/person")
+    @DisplayName("2 - Should throw a error when the id is not found")
+    void idNotFoundWhenEditingPerson() throws Exception {
+        PersonTestDto personTestDto = CreatePerson.createPersonTestDto();
+        mockMvc.perform(put("/person/" + 10552)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(person)))
+                        .content(new ObjectMapper().writeValueAsString(personTestDto)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("3 - Must throw a error without the birth date")
+    void badRequestWithoutBirthDate() throws Exception {
+        Person person = CreatePerson.createTestPerson();
+        personRepository.save(person);
+        final Person errorPerson = new Person("Bad Request");
+        mockMvc.perform(put("/person/" + person.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(errorPerson)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    @Order(3)
-    @DisplayName("3 - Must throw a error without the name")
+    @Order(4)
+    @DisplayName("4 - Must throw a error without the name")
     void badRequestWithoutName() throws Exception {
-        final PersonTestDto person = new PersonTestDto("1994-05-31");
-        mockMvc.perform(post("/person")
+        Person person = CreatePerson.createTestPerson();
+        personRepository.save(person);
+        final PersonTestDto errorPerson = new PersonTestDto("1994-05-31");
+        mockMvc.perform(put("/person/" + person.getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(person)))
+                        .content(new ObjectMapper().writeValueAsString(errorPerson)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
